4097b743e81a8c56458c501fcf72d0cb18262051
Alex Volo <alex_volo@bk.ru>
2023-10-09 10:29:52 +0900

    fix: ESlint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import * as readline from 'readline-sync';
 
-export function askUserName () {
-    const userName = readline.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+export default function askUserName() {
+  const userName = readline.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 }
